04f3110d8643a34b7f5aa85b53bbe3480d9a3d69
thewardoctor <email@email.com>
2023-05-04 16:20:31 +1000

    more filling out of librs

--- src/lib.rs
+++ src/lib.rs
@@ -5,12 +5,43 @@ struct Post {
 
 impl Post {
     fn new() -> Post {
+        Post {
+            state: Some(Box::new(Draft{})),
+            content: String::new()
+        }
+    }
 
+    fn add_text(&mut self, text: &str) {
+        self.content.push_str(text);
+    }
+
+    fn content(&self) -> &str {
+        ""
+    }
+
+    fn request_review(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.request_review())
+        }
     }
 }
 
-trait State {}
+trait State {
+    fn request_review(self: Box<Self>) -> Box<dyn State>;
+}
 
 struct Draft {}
 
-impl State for Draft {}
+impl State for Draft {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        Box::new(PendingReview{})
+    }
+}
+
+struct PendingReview {}
+
+impl State for PendingReview {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+}
