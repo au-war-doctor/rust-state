4c562d12411fb84a60e3cb4b73c79474758e7d50
thewardoctor <email@email.com>
2023-05-04 15:44:05 +1000

    TDD approach of publish states usage in main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use blog::Post;
+
 fn main() {
-    println!("Hello, world!");
+
+    // States: empty draft, review, approved?
+
+    post.add_text("I ate a salad for lunch, how interesting to my viewership");
+    assert_eq!("", post.content());
+
+    post.request_review();
+    assert_eq!("", post.content());
+
+    post.approve();
+    assert_eq!("I ate a salad for lunch, how interesting to my viewership", post.content());
 }
